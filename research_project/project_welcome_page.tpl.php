28436b6939402ff8b858daac3bc7121a5a8d1062
miyula1988@gmail.com <miyula@miyula-ThinkPad-T420.(none)>
2011-08-30 15:22:19 +0300

    add project welcome page

---
+++ research_project/project_welcome_page.tpl.php
@@ -0,0 +1,66 @@
+<?php
+// $Id$
+/**
+ * Template to display project welcome page.
+ *
+ * Fields available:
+ * $node: research project node object
+ * $module_path: the path of module folder
+ */
+?>
+<!-- start part of Research tools -->
+<div id="research-tools-div">
+<h3>Research tools</h3>
+<div class="research-tool-blocks">
+    <div class="research-tool-block">
+        <img class="research-tool-logo" src="http://127.0.0.1/drupal6/themes/garland/logo.png" alt=""/>
+        <p class="research-tool-name-p"></p>
+        <ul>
+            <li>Read more</li>
+            <li>View data</li>
+        </ul>
+    </div>
+    <div class="research-tool-block">
+        <img src="" alt=""/>
+        <p class="research-tool-name-p"></p>
+        <ul>
+            <li>Read more</li>
+            <li>View data</li>
+        </ul>
+    </div>
+    <div class="research-tool-block">
+        <img src="" alt=""/>
+        <p class="research-tool-name-p"></p>
+        <ul>
+            <li>Read more</li>
+            <li>View data</li>
+        </ul>
+    </div>
+    <div class="research-tool-block">
+        <img src="" alt=""/>
+        <p class="research-tool-name-p"></p>
+        <ul>
+            <li>Read more</li>
+            <li>View data</li>
+        </ul>
+    </div>
+</div>
+</div>
+<!-- end part of Research tools -->
+
+<!-- start part of Recent news -->
+<div id="recent-news-div">
+<h3>Recent News</h3>
+<ul>
+    <li><span class="news-title-span"><a href="">I am the title</a></span><span class="news-time-span">31/08/2011</span></li>
+    <li><span class="news-title-span"><a href="">I am the title</a></span><span class="news-time-span">31/08/2011</span></li>
+    <li><span class="news-title-span"><a href="">I am the title</a></span><span class="news-time-span">31/08/2011</span></li>
+</ul>
+</div>
+<!-- end part of Recent news -->
+
+<!-- start part of join link -->
+<div id="join-link-div">
+    <p><a href="">I want to join</a></p>
+</div>
+<!-- end part of join link -->
