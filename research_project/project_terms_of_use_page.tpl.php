4153e1393d9e10a5f0f27fd1ce0e153c5e041e73
yongzhen zhang <4.m@live.cn>
2011-08-30 15:42:32 +0300

    term of use

---
+++ research_project/project_terms_of_use_page.tpl.php
@@ -0,0 +1,19 @@
+<?php
+// $Id$
+/**
+ * Template to display user profile page.
+ *
+ * Fields available:
+ * $person: user object 
+ * $module_path: the path of module folder in the syetem
+ */
+?>
+<div id="user-card">
+    <div id="user-card-left">
+        <dl>
+            <dt>Email</dt>
+            <dd><?php echo $terms_template;?></dd>
+        </dl>
+    
+    </div>
+</div>
